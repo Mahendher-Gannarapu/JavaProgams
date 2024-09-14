1d47969153d50b1dafd85e38e233eeeaef3bd01b
Mahendher-Gannarapu <gannarapu.mahendher99@gmail.com>
2024-09-14 15:20:12 +0530

    Add the files

---
+++ src/Sept_13092024/StringEx.java
@@ -0,0 +1,16 @@
+package Sept_13092024;
+
+import java.sql.SQLOutput;
+
+public class StringEx {
+    public static void main(String[] args) {
+        String s1= "Mahendher";
+        String s2= "gannarapu";
+
+        System.out.println(s1+ " learning ATB class with "+s2);
+
+        //String formatting
+        System.out.printf("%s is learning Automation with %s",s1,s2);
+
+    }
+}
