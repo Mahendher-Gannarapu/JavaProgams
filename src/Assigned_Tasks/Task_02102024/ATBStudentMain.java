d562bc8f1ac1de6516eb2baf48c6b07227312349
Mahendher-Gannarapu <gannarapu.mahendher99@gmail.com>
2024-10-20 21:16:18 +0530

    Add the files

---
+++ src/Assigned_Tasks/Task_02102024/ATBStudentMain.java
@@ -0,0 +1,11 @@
+package Assigned_Tasks.Task_02102024;
+
+public class ATBStudentMain {
+           public static void main(String[] args) {
+            ATBStudent atb = new ATBStudent();
+            atb.firstname = "Gannarapu";
+            atb.attend_live_class();
+            atb.do_task();
+            atb.preparation();
+       }
+}
---
+++ src/Assigned_Tasks/Task_02102024/Class_Objects_5Points.java
@@ -0,0 +1,42 @@
+package Assigned_Tasks.Task_02102024;
+
+public class Class_Objects_5Points {
+    public static void main(String[] args) {
+
+// Write 5 points related to the Class and Objects.
+/*
+Class:
+1. Class is a blueprint or template that defines the structure and behavior of objects
+2. Class contains
+a) Data members | Attributes | instance variables | fields | member variables
+b) Member functions | Behaviour | Methods
+3. It represents the properties and actions of an object.
+4. Classes can use access modifiers (public, private, protected, and package-private)
+to control the visibility and accessibility of their fields and methods
+5. A class can have constructors, which are special methods used to initialize objects.
+
+Object:
+1. Instance of a Class
+An object is an instance of a class. It is created using the new keyword,
+and represents a specific entity with its own unique state based on the fields and
+behaviors defined in the class.
+2. State and Behavior
+Objects have two primary characteristics: state and behavior.
+The state of an object is defined by its fields (variables),
+while its behavior is defined by the methods (functions) in the class.
+3. Memory Allocation
+When an object is created, memory is allocated to store its state (field values).
+Each object has its own separate copy of the fields,
+but all instances of a class share the same method definitions.
+4. Reference Variable
+In Java, objects are accessed through reference variables.
+A reference variable holds the memory address of the object.
+For example, Car myCar = new Car(); creates an object of type Car and myCar refers to it.
+5. Object Lifecycle
+An object in Java is created using the new keyword and lives in the heap memory.
+Once it is no longer referenced by any part of the program,
+it becomes eligible for garbage collection, which automatically reclaims memory
+used by objects no longer in use.
+*/
+    }
+}
