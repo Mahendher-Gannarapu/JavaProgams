ab09330569aec87cc98e1311ec44175da240828d
Mahendher <gannarapu.mahendher99@gmail.com>
2026-02-12 22:25:23 +0530

    Add

--- src/October_2024/_11102024_Encapsulation/Ex2/MainDemo.java
+++ src/October_2024/_11102024_Encapsulation/Ex2/MainDemo.java
@@ -16,3 +16,22 @@ public class MainDemo {
         System.out.println(admin.getName());
     }
 }
+
+/*
+    public static void main(String[] args) {
+        ICICIBank amit = new ICICIBank("Amit", 10000);
+        // amit.bal =100; //bal and name are private so not allowed
+        // amit.setBal(1111111);
+        // This should allow only by admin
+
+        //Write a code to connect with mySQL
+        //Admin
+
+        boolean are_you_admin =true;
+        ICICIBank admin = new ICICIBank("admin", 22222);
+        admin.setBal(55555, true);
+        System.out.println(admin.getName());
+    }
+}
+
+ */
